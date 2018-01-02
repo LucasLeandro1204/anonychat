9066bf8049aa5f602d140673cf388f740a10c82b
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-01-01 22:22:25 -0200

    mix config

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Anonychat</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+  <div id="app"></div>
+  <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,38 @@
+const glob = require('glob-all');
 const mix = require('laravel-mix');
-
+const tailwindcss = require('tailwindcss');
+const purgecssPlugin = require('purgecss-webpack-plugin');
 
 mix.js('resources/assets/js/app.js', 'public/js')
-  .sass('resources/assets/sass/app.scss', 'public/css');
+  .options({
+    extractVueStyles: 'public/css/app.css',
+    postCss: [
+      tailwindcss('./tailwind.config.js'),
+    ]
+  });
+
+if (! mix.inProduction()) {
+  return;
+}
+
+mix.webpackConfig({
+  plugins: [
+    new purgecssPlugin({
+      paths: glob.sync([
+        path.join(__dirname, 'resources/assets/js/**/*.vue')
+      ]),
+
+      extractors: [
+        {
+          extractor: class {
+            static extract(content) {
+              return content.match(/[A-z0-9-:\/]+/g) || [];
+            }
+          },
+
+          extensions: ['html', 'js', 'vue']
+        }
+      ]
+    })
+  ]
+});
