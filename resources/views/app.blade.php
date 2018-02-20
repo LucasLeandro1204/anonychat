d0baec25230eed8a004d1fdef2a44b6c52e477e8
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-02-20 17:10:51 -0300

    base

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Anonychat</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Dosis:500|Roboto:300,400,500" rel="stylesheet">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+  <div id="app"></div>
+  <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
